5fa7cbab5e54accbe08408b8b2c5481253214821
Nam Nguyen <nam.nguyen201@gmail.com>
2023-03-06 16:55:22 -0800

    Serve up static files in client/dist directory

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ let app = express();
 // Set up static file service for files in the `client/dist` directory.
 // Webpack is configured to generate files in that directory and
 // this server must serve those files when requested.
+app.use(express.static('./client/dist'));
 
 app.post('/repos', function (req, res) {
   // TODO - your code here!
@@ -15,6 +16,7 @@ app.post('/repos', function (req, res) {
 
 app.get('/repos', function (req, res) {
   // TODO - your code here!
+  console.log('hello from server');
   // This route should send back the top 25 repos
 });
 
